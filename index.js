98ae798e64fa09d5e0c2a06be15972190461519a
matthew lee <matthewlee07@gmail.com>
2017-11-17 11:39:58 -0500

    working get and delete

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const {PORT, CLIENT_ORIGIN} = require('./config');
-const {dbConnect} = require('./db-mongoose');
+const { PORT, CLIENT_ORIGIN } = require('./config');
+// const { dbConnect } = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
 const app = express();
+const allCats = [
+    {
+        imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+        imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+        name: 'Fluffy',
+        gender: 'Female',
+        age: 2,
+        breed: 'Bengal',
+        story: 'Thrown on the street'
+    }
+];
+const allDogs = [
+    {
+        imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+        name: 'Zeus',
+        gender: 'Male',
+        age: 3,
+        breed: 'Golden Retriever',
+        story: 'Owner Passed away'
+    }, 
+    {
+        imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+        imageDescription: 'testing',
+        name: 'Zeus',
+        gender: 'Male',
+        age: 3,
+        breed: 'Golden Retriever',
+        story: 'Owner Passed away'
+    }
+];
 
 app.use(
     morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -20,6 +51,22 @@ app.use(
     })
 );
 
+app.get('/api/cat', (req, res) => {
+    res.json(allCats[allCats.length - 1]);
+})
+
+app.get('/api/dog', (req, res) => {
+    res.json(allDogs[allDogs.length -1]);
+})
+
+app.delete('/api/cat', (req, res) => {
+    res.json(allDogs.pop())
+})
+
+app.delete('/api/dog', (req, res) => {
+    res.json(allDogs.pop())
+})
+
 function runServer(port = PORT) {
     const server = app
         .listen(port, () => {
@@ -32,8 +79,8 @@ function runServer(port = PORT) {
 }
 
 if (require.main === module) {
-    dbConnect();
+    // dbConnect();
     runServer();
 }
 
-module.exports = {app};
+module.exports = { app };
